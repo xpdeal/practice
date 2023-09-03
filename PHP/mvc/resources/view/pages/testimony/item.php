e92198db0d6e81f55ac751047bf3961bb2d14995
Elvis Leite <elvis7t@gmail.com>
2023-09-03 10:52:00 -0300

    refactor(new-product-flags) removed imagem added css class

---
+++ PHP/mvc/resources/view/pages/testimony/item.php
@@ -0,0 +1,9 @@
+<section id="testmonies">
+    <div class="card text-dark mb-3">
+        <h5 class="card-header">{{name}} <small>{{date}}</small></h5>
+        <div class="card_body">
+            {{message}}
+        </div>
+    </div>
+
+</section>
