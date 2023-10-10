ba5c4e097e5e523cdcfa4b9ed2f4e32c429a0102
Elvis Leite <elvis7t@gmail.com>
2023-10-10 08:27:48 -0300

    feat(open-table-pdv) added actionOpenOrder

--- JS/Cursos/useacabeca-js/js/irock.js
+++ JS/Cursos/useacabeca-js/js/irock.js
@@ -15,26 +15,40 @@ function hello() {
 
 
 function touchRock() {
-    var id = document.querySelector(".rock");
-    var user = prompt("Qual seu nome parça?", "Enter ypur name here");
+    var id = document.querySelector(".rock");    
+    var user = prompt("Qual seu nome parça?", "Enter your name here");
+    setCookie(user);
     if (user) {
-        alert("ènois mano", + user.textContent + ".");
+        alert("è nois mano "+ user);
         id.classList.add("smile");
     }
+    console.log(user);
+    
 
 }
 
 function setTime() {
-    
+
     setInterval(() => {
-        alert("no");
+        alert("Não me ama!!!");
         var det = document.getElementById("test");
         det.classList.remove("visually-hidden");
     }, 10000);
 }
 
-function resizeRock(){
-
+function resizeRock() {
     document.getElementById("image").style.height = (document.body.clientHeight - 100) * 9;
-
 }
+
+
+    const setCookie = (value) => {
+        const cookieName = "nome_user";
+        const cookieValue = encodeURIComponent(value);
+        const expirationDate = new Date();
+        const sameSite = "SameSite=restrict";
+        const secure = "Secure";
+        expirationDate.setTime(expirationDate.getTime() + 24 * 60 * 60 * 1000);
+        document.cookie = `${cookieName}=${cookieValue};expires=${expirationDate.toUTCString()};${sameSite};${secure}`;    
+    }
+    
+    console.log(setCookie)
